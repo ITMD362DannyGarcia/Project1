e19fb3313ee3c420b464af945ede61c8cc9af227
dgarcia123 <32852331+dgarcia123@users.noreply.github.com>
2020-03-25 15:35:51 -0500

    Added validation for every field on the form

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ function magic(){
 
 
   var email = document.forms["myForm"]["email"].value;
+  var fname = document.forms["myForm"]["fname"].value;
+  var lname = document.forms["myForm"]["lname"].value;
+  var phone = document.forms["myForm"]["phone"].value;
+  var date = document.forms["myForm"]["date"].value;
 
-  if (email == "") {
-    alert("Remember to enter your email!");
+  if ((email == "") || (fname == "") || (lname == "") || (phone == "") || (date == "") ) {
+    alert("Remember to fill in all fields!");
     return false;
+
   } else{
     alert("Great, you're all signed up for the beta!")
   }
